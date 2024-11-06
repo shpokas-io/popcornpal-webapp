58da8eae304254b410ee08884238971230a4064d
shpokas <s.shpokas@gmail.com>
2024-11-06 09:52:51 +0200

    feat: Add dynamic welcome message and centered logo on homepage

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,18 +2,38 @@ import { Typography, Box, Button } from "@mui/material";
 import { useAppDispatch } from "../hooks/hooks";
 import { logout } from "../features/auth/authSlice";
 import NavBar from "../components/NavBar";
+import logo from "../assets/images/logo-nobc.png";
 
 const MainPage: React.FC = () => {
   const dispatch = useAppDispatch();
+
   const handleLogout = () => {
     dispatch(logout());
   };
+
+  //Array of dynamic welcome messages
+  const welcomeMessages = [
+    "Welcome back, movie buff!",
+    "Ready to dive into some cinematic magic?",
+    "Lights, camera, action! Let’s find your next favorite movie.",
+    "Popcorn’s ready! Let's explore the movie world.",
+    "Ready for a binge-worthy experience?",
+  ];
+
+  //Randomly select one message
+  const randomMessage =
+    welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
 
   return (
     <>
       <NavBar />
       <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
-        <Typography variant="h4">Welcome to the main Page!</Typography>
+        <img
+          src={logo}
+          alt="App Logo"
+          style={{ width: "150px", marginBottom: "20px" }}
+        />
+        <Typography variant="h4">{randomMessage}</Typography>
         <Typography>Authenticated Content Here</Typography>
         <Button
           variant="contained"
