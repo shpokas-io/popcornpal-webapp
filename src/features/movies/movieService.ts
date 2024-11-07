d8b2a26f2a3ae579e209939059091770fef156fd
shpokas <s.shpokas@gmail.com>
2024-11-07 09:38:42 +0200

    Refresh .gitignore tracking

--- src/features/movies/movieService.ts
+++ src/features/movies/movieService.ts
@@ -1,31 +1,26 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 export const fetchMoviesApi = async () => {
-  const response = await axios.get("http://localhost:3000/movies");
+  const response = await axios.get(`${API_URL}/movies`);
   return response.data.data || [];
 };
 
 export const addFavoriteApi = async (movieId: string, token: string) => {
-  return await axios.post(
-    `http://localhost:3000/movies/${movieId}/favorite`,
-    null,
-    {
-      headers: { Authorization: `Bearer ${token}` },
-    }
-  );
+  return await axios.post(`${API_URL}/movies/${movieId}/favorite`, null, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 };
 
 export const removeFavoriteApi = async (movieId: string, token: string) => {
-  return await axios.delete(
-    `http://localhost:3000/movies/${movieId}/favorite`,
-    {
-      headers: { Authorization: `Bearer ${token}` },
-    }
-  );
+  return await axios.delete(`${API_URL}/movies/${movieId}/favorite`, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
 };
 
 export const fetchFavoritesApi = async (token: string) => {
-  const response = await axios.get("http://localhost:3000/movies/favorites", {
+  const response = await axios.get(`${API_URL}/movies/favorites`, {
     headers: { Authorization: `Bearer ${token}` },
   });
   return response.data.data;
