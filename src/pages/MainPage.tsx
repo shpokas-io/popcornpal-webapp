b3019b613a3a8d0d996a71f2fea11c4be88c2396
shpokas <s.shpokas@gmail.com>
2024-11-08 10:41:58 +0200

    Refactor: Extract welcome message into separate component

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,44 +1,11 @@
 import React from "react";
-import { Typography, Box } from "@mui/material";
-import logo from "../assets/images/logo-nobc.png";
+import WelcomeMessage from "../components/utils/WelcomeMessage";
 import MoviesCarousel from "../components/movies/MoviesCarousel";
 
 const MainPage: React.FC = () => {
-  const welcomeMessages = [
-    "Welcome back, Time to dive into someone else`s drama for a change.",
-    "Escaping reality, one movie at a time. We won’t tell.",
-    "Forget your problems—get lost in someone else’s scripted misery!",
-    "Let’s face it, the plot twists here are better than real life.",
-    "Welcome! Just remember, movie magic won’t fix real life… yet.",
-    "Reality is overrated. Let’s go somewhere fictional.",
-    "Need to forget the outside world? We’ve got just the ticket.",
-    "Movies: because reality is too real.",
-    "Here’s hoping today’s films are less disappointing than real life.",
-    "Escapism at its finest. Just… don’t look at the clock.",
-  ];
-
-  const randomMessage =
-    welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)];
-
   return (
     <>
-      <Box display="flex" flexDirection="column" alignItems="center" mt={4}>
-        <img
-          src={logo}
-          alt="App Logo"
-          style={{ width: "150px", marginBottom: "20px" }}
-        />
-        <Box
-          display="flex"
-          justifyContent="center"
-          textAlign="center"
-          width="90%"
-          mt={2}
-        >
-          <Typography variant="h4">{randomMessage}</Typography>
-        </Box>
-      </Box>
-
+      <WelcomeMessage />
       <MoviesCarousel />
     </>
   );
